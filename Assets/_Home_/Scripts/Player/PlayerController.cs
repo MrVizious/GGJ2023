253db8ec70a9576220471308942c084ac74f9135
Javier <mrvizious@gmail.com>
2023-02-04 01:03:14 +0100

    PlayerController beginning

---
+++ Assets/_Home_/Scripts/Player/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RuntimeSet;
+
+public class PlayerController : MonoBehaviour
+{
+    //TODO: Character class and its subsequent CharacterRuntimeSet SO class
+    //[SerializeField] private Character leftCharacter, rightCharacter;
+    //public CharacterRuntimeSet availableCharacters;
+    [SerializeField]
+    Color leftColor, rightColor;
+
+    [SerializeField] private PortraitController portraitController;
+}
