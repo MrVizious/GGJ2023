54e2f42f15ea58b05045ea43607baa96137284f0
SerghG <38987734+SerghG@users.noreply.github.com>
2023-02-04 20:14:46 +0100

    Update AudioController.cs

--- Assets/_Home_/Scripts/Audio/AudioController.cs
+++ Assets/_Home_/Scripts/Audio/AudioController.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DesignPatterns;
+using ExtensionMethods;
 
 public class AudioController : Singleton<AudioController>
 {
-    public AudioClip[] musicaBG = new AudioClip[4];
-    public AudioClip[] SoundsSFX = new AudioClip[10];
+    public List<AudioClip> musicaBG = new List<AudioClip>();
+    public List<AudioClip> SoundsSFX = new List<AudioClip>();
+    public List<AudioClip> AmbientSFXMedieval = new List<AudioClip>();
+    public List<AudioClip> AmbientSFXVictorian = new List<AudioClip>();
+    public List<AudioClip> AmbientSFXModern = new List<AudioClip>();
+    public List<AudioClip> AmbientSFXFuturistic = new List<AudioClip>();
+
+    public int Epoca = 0;
 
     public AudioSource SFXAudioSource;
     public AudioSource MusicAudioSource;
+    public AudioSource AmbientAudioSource;
 
     public static AudioController instance;
 
@@ -30,5 +38,23 @@ public class AudioController : Singleton<AudioController>
         MusicAudioSource.Play();
     }
 
+    private void PlayAmbientSound(){
+        if(Random.Range(0, 10.0f) < 7.0){
+            switch(Epoca){
+                case 0:
+                    AmbientAudioSource.PlayOneShot(AmbientSFXMedieval.GetRandomItem<AudioClip>());
+                    break;
+                case 1:
+                    AmbientAudioSource.PlayOneShot(AmbientSFXVictorian.GetRandomItem<AudioClip>());
+                    break;
+                case 2:
+                    AmbientAudioSource.PlayOneShot(AmbientSFXModern.GetRandomItem<AudioClip>());
+                    break;
+                case 3:
+                    AmbientAudioSource.PlayOneShot(AmbientSFXFuturistic.GetRandomItem<AudioClip>());
+                    break;
+            }
+        }
+    }
 
 }
